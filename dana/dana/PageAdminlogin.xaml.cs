06ab0818951770f66eb95958b8cb0ecc2c1dbe49
danahazem <danahazem875@gmail.com>
2023-12-05 10:08:38 +0200

    first commit

---
+++ dana/dana/PageAdminlogin.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace dana
+{
+    /// <summary>
+    /// Interaction logic for PageAdminlogin.xaml
+    /// </summary>
+    public partial class PageAdminlogin : Page
+    {
+        login_Entities1 db = new login_Entities1();
+        public PageAdminlogin()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+           Admin_ admin  = new Admin_();
+            admin = db.Admin_.First(x => x.admin_username == adminName.Text && x.admin_pass == adminpassword.Text);
+            MessageBox.Show("login is done");
+
+            PageSearch search  = new PageSearch();
+            this.NavigationService.Navigate(search);
+        }
+    }
+}
---
+++ dana/dana/PageSearch.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace dana
+{
+
+    /// <summary>
+    /// Interaction logic for PageSearch.xaml
+    /// </summary>
+    public partial class PageSearch : Page
+    {
+        login_Entities1 db = new login_Entities1();
+        public PageSearch()
+        {
+            InitializeComponent();
+            dg.ItemsSource = db.uesr_.ToList();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            uesr_ uesr = new uesr_();
+            dg.ItemsSource = db.uesr_.Where(x => x.City == search.Text);
+            db.uesr_.Select(x=>new{ x.Username , x.Age ,x.Password_,x.Gender ,x.Phone_number,x.City,);
+            db.SaveChanges();
+          
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            uesr_ uesr = new uesr_();
+            dg.ItemsSource = db.uesr_.Where(x => x.Phone_number == remove.Text); 
+            db.uesr_.Remove(uesr);
+            db.SaveChanges();
+        }
+    }
+
+        
+    }
+}
